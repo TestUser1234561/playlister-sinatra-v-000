ed0a5d4419df61a1711084de7b6c6315349c2cad
Morgan Fudge <mfudge92@gmail.com>
2018-05-15 07:11:56 +0000

    automatically backed up by learn

--- db/migrate/20180203133533_create_artists.rb
+++ db/migrate/20180203133533_create_artists.rb
@@ -12,7 +12,13 @@ class CreateArtists < ActiveRecord::Migration
 
     create_table :songs do |t|
       t.string :name
+      t.integer :genre_id
       t.belongs_to :artist, index: true
+    end
+
+    create_table :song_genres do |t|
+      t.integer :song_id
+      t.integer :genre_id
     end
   end
 end
